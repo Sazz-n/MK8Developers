07f3805c0a209ae2c1c845814e7b6d7c4feb8ec3
maiha-mk <my82.hani@gmail.com>
2022-07-31 18:25:19 +0900

    some fix

--- cogs/basic.py
+++ cogs/basic.py
@@ -1,8 +1,28 @@
+import discord
 from discord import app_commands, Embed, Interaction, Object
 from discord.ext import commands
+from typing import Optional
 
 class Basic(commands.Cog):
     
+    def __init__(self, bot:commands.Bot):
+        self.bot = bot
+
+    @app_commands.command(name="embed", description="embed(埋め込みテキスト)の作成")
+    @app_commands.describe(title="タイトル", description="内容", author="作者")
+    async def makeembed(
+        self,
+        interaction: Interaction,
+        title: str,
+        description: str,
+        author: Optional[discord.Member] = None
+        ):
+        embed = Embed()
+        embed.title = title
+        embed.description = description
+        embed.set_author(name=author, icon_url=author.display_avatar.url)
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+
     @app_commands.command(name="help", description="ヘルプの表示")
     async def help(self, interaction:Interaction):
         embed = Embed(title="ヘルプ")
