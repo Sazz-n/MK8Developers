e99d81c9d693725087b3fe85c2472eb1312cf0ba
maiha-mk <my82.hani@gmail.com>
2022-07-31 12:59:07 +0900

    add Bot class

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ from discord.ext import commands
 import config
 
 intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
 
+class Bot(commands.Bot):
+
+    def __init__(self):
+        super().__init__(command_prefix="!", intents=intents, application_id=997868018828845056)
+        self.initial_extensions = ["Basic.cog"]
+        
+    async def setup_hook(self):
+        for extension in self.initial_extensions:
+            await self.load_extension(extension)
+        await bot.tree.sync(guild=discord.Object(id=965811655911022602)) #guildは公開時に削除
+        
+bot = Bot()
 bot.run(config.TOKEN)
