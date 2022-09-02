d02933ffcac58c4b08719eb4f80764bb47917612
maiha-mk <my82.hani@gmail.com>
2022-09-02 09:54:04 +0900

    add cogs.sokuji in extensions

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Bot(commands.Bot):
 
     def __init__(self):
         super().__init__(command_prefix="!", intents=intents, application_id=997868018828845056)
-        self.initial_extensions = ["cogs.basic"]
+        self.initial_extensions = ["cogs.basic", "cogs.sokuji"]
         
     async def setup_hook(self):
         for extension in self.initial_extensions:
