8e89f619c6761b52f33ba338e909126b8c3aa6eb
maiha-mk <my82.hani@gmail.com>
2022-09-04 14:48:11 +0900

    add poll command

--- cogs/basic.py
+++ cogs/basic.py
@@ -1,8 +1,10 @@
 import discord
-from discord import app_commands, Embed, Interaction, Object, Color
+from discord import app_commands, Embed, Interaction, Object, Color, TextStyle
+from discord.ui import Modal, TextInput
 from discord.app_commands import Choice
 from discord.ext import commands
 from typing import Optional
+import re
 
 color_dic = {
     "red":Color.red(), 
@@ -15,6 +17,35 @@ color_dic = {
 class Basic(commands.Cog):
     def __init__(self, bot:commands.Bot):
         self.bot = bot
+
+    @app_commands.command(name="poll", description="投票の作成")
+    async def poll(self, interaction: Interaction):
+        await interaction.response.send_modal(Basic.PollModal(bot=self.bot))
+
+    class PollModal(Modal):
+        def __init__(self, bot:commands.Bot):
+            self.bot = bot
+            super().__init__(timeout=None, title="投票の作成")
+        daimei = TextInput(label="題名")
+        choices = TextInput(label="選択肢 1つにつき1行(最大10個)", style=TextStyle.paragraph)
+        async def on_submit(self, interaction: Interaction):
+            choice_li = re.split('\n', self.choices.value)
+            if len(choice_li) < 11:
+                await interaction.response.send_message(embed=Basic.PollEmbed(title=self.daimei.value, choice=choice_li))
+                msg = await interaction.original_response()
+                emoji = ["0️⃣","1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣","7️⃣","8️⃣","9️⃣"]
+                for i in choice_li:
+                    await msg.add_reaction(emoji[choice_li.index(i)])
+            else:
+                await interaction.response.send_message("選択肢は10個以内にしてください", ephemeral=True)
+
+    class PollEmbed(Embed):
+        def __init__(self, title, choice: list):
+            emoji = [":zero:",":one:",":two:",":three:",":four:",":five:",":six:",":seven:",":eight:",":nine:"]
+            description = ""
+            for i in choice:
+                description += f"{emoji[choice.index(i)]} {i}\n"
+            super().__init__(color=Color.green(), title=title, description=description)
 
     @app_commands.command(name="embed", description="embed(埋め込みテキスト)の作成")
     @app_commands.describe(title="タイトル", description="内容", color="色", author="作者")
